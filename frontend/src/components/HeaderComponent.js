a2729167eff070e8e41b68e28849eea228074868
Aryan_19 <an2013.ak@gmail.com>
2022-12-30 16:20:29 +0530

    develop

--- frontend/src/components/HeaderComponent.js
+++ frontend/src/components/HeaderComponent.js
@@ -2,10 +2,11 @@ import { Navbar, Nav, Container, NavDropdown, Badge, Form, Dropdown, DropdownBut
 import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 import { logout } from "../redux/actions/userActions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const HeaderComponent = () => {
     const dispatch = useDispatch()
+    const { userInfo } = useSelector((state) => state.userRegisterLogin)
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
@@ -28,42 +29,53 @@ const HeaderComponent = () => {
                         </InputGroup>
                     </Nav>
                     <Nav>
-                        <LinkContainer to="/admin/orders">
-                            <Nav.Link>
-                                Admin
-                                <span className="position-absolute top-1 start-10 translate-middle p-2 bg-danger border border-light
-                        rounded-circle"></span>
-                            </Nav.Link>
+                        {userInfo.isAdmin ? (
+                            <LinkContainer to="/admin/orders">
+                                <Nav.Link>
+                                    Admin
+                                    <span className="position-absolute top-1 start-10 translate-middle p-2 bg-danger border border-light
+                         rounded-circle"></span>
+                                </Nav.Link>
 
-                        </LinkContainer>
+                            </LinkContainer>
+
+                        ) : userInfo.name && !userInfo.isAdmin ? (
+                            <NavDropdown title={`${userInfo.name} ${userInfo.lastName}`} id="collasible-nav-dropdown">
+                                <NavDropdown.Item eventKey="/user/my-orders" as={Link} to="/user/my-orders">
+                                    My orders
+                                </NavDropdown.Item>
+                                <NavDropdown.Item eventKey="/user" as={Link} to="/user">
+                                    My profile
+                                </NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => dispatch(logout())}>
+                                    Logout
+                                </NavDropdown.Item>
+
+                            </NavDropdown>
+
+                        ) : (
+                            <>
+                                <LinkContainer to="/login">
+                                    <Nav.Link>
+                                        Login
+
+                                    </Nav.Link>
+
+                                </LinkContainer>
+                                <LinkContainer to="/register">
+                                    <Nav.Link>
+                                        Register
+
+                                    </Nav.Link>
+
+                                </LinkContainer>
+                            </>
+                        )}
 
 
-                        <NavDropdown title="Aryan" id="collasible-nav-dropdown">
-                            <NavDropdown.Item eventKey="/user/my-orders" as={Link} to="/user/my-orders">
-                                My orders
-                            </NavDropdown.Item>
-                            <NavDropdown.Item eventKey="/user" as={Link} to="/user">
-                                My profile
-                            </NavDropdown.Item>
-                            <NavDropdown.Item onClick={()=>dispatch(logout())}>
-                                Logout
-                            </NavDropdown.Item>
 
-                        </NavDropdown>
-                        <LinkContainer to="/login">
-                            <Nav.Link>
-                                Login
 
-                            </Nav.Link>
 
-                        </LinkContainer>
-                        <LinkContainer to="/register">
-                            <Nav.Link>
-                                Register
-
-                            </Nav.Link>
-
-                        </LinkContainer>
                         <LinkContainer to="/cart">
                             <Nav.Link>
                                 <Badge pill bg="danger">
