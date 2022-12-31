153c76d386e503669834f99337f56d3902e53f3d
Aryan_19 <an2013.ak@gmail.com>
2022-12-31 19:14:36 +0530

    develop

--- frontend/src/pages/user/UserProfilePage.js
+++ frontend/src/pages/user/UserProfilePage.js
@@ -1,25 +1,34 @@
 import UserProfilePageComponent from "./components/UserProfilePageComponent";
 import axios from 'axios'
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { setReduxUserState } from "../../redux/actions/userActions";
 
-const updateUserApiRequest = async(name, lastName, phoneNumber, address, country, zipCode, city, state, password) => {
+const updateUserApiRequest = async (name, lastName, phoneNumber, address, country, zipCode, city, state, password) => {
   const { data } = await axios.put(
-      '/api/users/profile',
-      { name, lastName, phoneNumber, address, country, zipCode, city, state, password },
+    '/api/users/profile',
+    { name, lastName, phoneNumber, address, country, zipCode, city, state, password },
   )
   return data
 }
-const fetchUser = async(user_id)=>{
-  const {data}=await axios.get("/api/users/profile/"+user_id)
+const fetchUser = async (id) => {
+  const { data } = await axios.get("/api/users/profile/" + id)
   return data
 }
 
 
 const UserProfilePage = () => {
 
-  const {userInfo} = useSelector((state)=>state.userRegisterLogin)
-  return <UserProfilePageComponent updateUserApiRequest={updateUserApiRequest} fetchUser={fetchUser}
-  userInfo={userInfo} />
+  const reduxDispatch = useDispatch()
+  const { userInfo } = useSelector((state) => state.userRegisterLogin)
+  return <UserProfilePageComponent
+    updateUserApiRequest={updateUserApiRequest}
+    fetchUser={fetchUser}
+    userInfoFromRedux={userInfo}
+    setReduxUserState={setReduxUserState}
+    reduxDispatch={reduxDispatch}
+    localStorage={window.localStorage}
+    sessionStorage={window.sessionStorage}
+  />
 };
 
 export default UserProfilePage;
