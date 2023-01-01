0d6ba791e8f8161e172947bcb620d8f026cfc773
Aryan_19 <an2013.ak@gmail.com>
2023-01-02 01:51:09 +0530

    develop

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,7 +1,9 @@
 import * as actionTypes from "../constants/cartConstants"
 
-export const addToCart=()=>(dispatch)=>{//this fn will be passed to reducer fn. which will
+export const addToCart=(productId, quantity)=>(dispatch)=>{//this fn will be passed to reducer fn. which will
     //actually change the state
+    console.log(productId)
+    console.log(quantity)
     dispatch({
         type: actionTypes.ADD_TO_CART,
         someValue: 0,
