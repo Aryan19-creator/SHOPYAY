301173167f0cd4d629f5e22d556db7a4324b9248
Aryan_19 <an2013.ak@gmail.com>
2023-01-12 16:33:27 +0530

    analytic page using socket.io

--- backend/models/OrderModel.js
+++ backend/models/OrderModel.js
@@ -1,5 +1,5 @@
-const { default: mongoose } = require("mongoose")
-const User=require("./UserModel")
+const mongoose = require("mongoose")
+const User = require("./UserModel")
 
 const orderSchema = mongoose.Schema({
     user: {
@@ -9,40 +9,40 @@ const orderSchema = mongoose.Schema({
     },
     orderTotal: {
         itemsCount: {type: Number, required: true},
-        cartSubtotal: {type:Number, required: true}
+        cartSubtotal: {type: Number, required: true}
     },
-    cartItems:[
+    cartItems: [
         {
-            name:{type:String, required: true},
-            price:{type:Number, required: true},
-            image:{path: {type:String, required: true}},
-            quantity:{type: Number, required: true},
-            count:{type: Number, required: true}
+            name: {type: String, required: true},
+            price: {type: Number, required: true},
+            image: {path: {type: String, required: true}},
+            quantity: {type: Number, required: true},
+            count: {type: Number, required: true}
         }
     ],
     paymentMethod: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     transactionResult: {
-        status: {type:String},
-        createTime: {type:String},
+        status: {type: String},
+        createTime: {type: String},
         amount: {type: Number}
     },
-    isPaid:{
-        type:Boolean,
-        required: true,
-        default: false,
-    },
-    paidAt:{
-        type: Date,
-    },
-    isDelivered:{
+    isPaid: {
         type: Boolean,
         required: true,
         default: false,
     },
-    deliveredAt:{
+    paidAt: {
+        type: Date,
+    },
+    isDelivered: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    deliveredAt: {
         type: Date,
     }
 }, {
@@ -50,4 +50,10 @@ const orderSchema = mongoose.Schema({
 })
 
 const Order = mongoose.model("Order", orderSchema)
+Order.watch().on("change", (data) => {
+   
+    if (data.operationType === "insert") {
+        io.emit("newOrder", data.fullDocument);
+    }
+})
 module.exports = Order
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const express = require("express");
-const fileUpload = require("express-fileupload")
-const cookieParser = require("cookie-parser")
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 const app = express();
-const port = 5001;
 
-app.use(express.json())//it is used to identify the json doc by express server which i am trying to send via postman 
-app.use(cookieParser())
-app.use(fileUpload())
+const httpServer = createServer(app);
+global.io = new Server(httpServer);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
 
 const apiRoutes = require("./routes/apiRoutes");
 
@@ -21,26 +25,24 @@ connectDB();
 app.use("/api", apiRoutes);
 
 app.use((error, req, res, next) => {
-  if(process.env.NODE_ENV === "development"){
-  console.error(error);
-}
-  next(error)
+  if (process.env.NODE_ENV === "development") {
+    console.error(error);
+  }
+  next(error);
 });
 app.use((error, req, res, next) => {
-  if(process.env.NODE_ENV === "development"){
+  if (process.env.NODE_ENV === "development") {
     res.status(500).json({
       message: error.message,
       stack: error.stack,
     });
-  }else {
-    res.status(500).json({
-      message: error.message
-    })
+  } else {
+      res.status(500).json({
+         message: error.message, 
+      })
   }
-  
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const PORT = process.env.PORT || 5001;
 
+httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
