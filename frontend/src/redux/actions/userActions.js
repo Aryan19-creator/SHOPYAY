09e79c2242889de4d076f85ad1477ac57ec8df2f
Aryan_19 <an2013.ak@gmail.com>
2023-01-13 17:05:05 +0530

    deploy

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -1,5 +1,7 @@
 import { LOGIN_USER, LOGOUT_USER } from "../constants/userConstants";
 import axios from "axios";
+import { useNavigation } from "react-router-dom";
+
 
 export const setReduxUserState = (userCreated)=>(dispatch)=>{
     dispatch({
@@ -9,7 +11,8 @@ export const setReduxUserState = (userCreated)=>(dispatch)=>{
 }
 
 export const logout=()=>(dispatch)=>{
-    this.props.history.push("/login");
+    const navigate=useNavigation();
+    navigate("/login");
     axios.get('/api/logout')
     localStorage.removeItem("userInfo")
     sessionStorage.removeItem("userInfo")
