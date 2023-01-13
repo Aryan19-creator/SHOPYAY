dea7f4bb4ee9d22e3d0300e6e9b873216c685b49
Aryan_19 <an2013.ak@gmail.com>
2023-01-14 01:01:50 +0530

    deploy

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -11,7 +11,7 @@ export const setReduxUserState = (userCreated)=>(dispatch)=>{
 }
 
 export const logout=()=>(dispatch)=>{
-    window.open('/login', '_blank')
+    setTimeout(function(){document.location.href = "/login"},500);
     axios.get('/api/logout')
     localStorage.removeItem("userInfo")
     sessionStorage.removeItem("userInfo")
